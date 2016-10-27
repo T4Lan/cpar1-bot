7cc35649a33a65d64244e82c9df8d87748cc77bd
Mauricio Bazán <mauriciodbazan@gmail.com>
2016-10-26 23:45:18 -0300

    Added protips.

--- bot.py
+++ bot.py
@@ -3,17 +3,48 @@ import time
 import telepot
 import re
 from pprint import pprint
-import markovify
+
+protips = []
+
+def persistMessage(msg):
+     f = open('Data/protips', 'a+')
+     f.write(msg)
+     f.write("\n")
+     f.close()
 
 def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     pprint(msg)
-
     if content_type == 'text':
-        if msg['text'] == 'quiensos':
-            bot.sendMessage(chat_id, "Soy el bot de la Campus Party Argentina y estoy para ayudarte!")
-        if msg['text'] == 'plano':
-            bot.sendPhoto(chat_id, "http://argentina.campus-party.org/files/large/89cc483747597b3")
+        matchObj = re.match("^campusbot (.*)", msg['text'])
+        if matchObj:
+            command = matchObj.group(1)
+            command.lower()
+            fromUser = msg['from']['username']
+            print "[Command] ", command, " from ", fromUser
+            if command == 'plano':
+                bot.sendPhoto(chat_id, "http://argentina.campus-party.org/files/large/89cc483747597b3")
+            if command == 'quien sos':
+                bot.sendMessage(chat_id, "Soy el bot de la Campus Party Argentina y estoy para ayudarte!")
+            if command == 'como llego':
+                bot.sendMessage(chat_id, "Soy el bot de la Campus Party Argentina y estoy para ayudarte!")
+            if command == 'protips':
+                response = ""
+                for protip in protips:
+                    response+=protip
+                    response+="\n"
+                    bot.sendMessage(chat_id,response)
+
+        else:
+            protipMatch = re.match("^.*#protip(.*)", msg['text'])
+            if protipMatch:
+                protip = protipMatch.group(1)
+                print "Protip: {}".format(protip)
+                protips.append(protip)
+                persistMessage(protip)
+
+        if msg['text'] == 'whoareyou':
+            bot.sendMessage(chat_id, "I'm a Bot programmed in Python, and y'all suck.")
         if msg['text'] == 'guia':
             bot.sendPhoto(chat_id, "http://argentina.campus-party.org/files/large/006225d505808e6")
         
